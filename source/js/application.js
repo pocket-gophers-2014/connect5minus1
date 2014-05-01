a70d3149009ecc278a27bf3a8ebeed3a9b22de08
Szarko <kingmitch@gmail.com>
2014-05-01 11:41:51 -0700

    Directories

---
+++ source/js/application.js
@@ -0,0 +1,15 @@
+// connect5minus1 Game Controller
+
+$(document).ready(function() {
+
+})
+
+// GameController base
+function gameController(view,board) {
+  view = new view
+  board = new board
+}
+
+gameController.prototype = {
+  
+}
