a2536e3926993a1a92595dc909958594a57fccfb
Szarko <kingmitch@gmail.com>
2014-05-01 12:36:56 -0700

    dat game Controller slowly

--- source/js/application.js
+++ source/js/application.js
@@ -1,15 +1,35 @@
 // connect5minus1 Game Controller
 
+// set Listeners
 $(document).ready(function() {
-
+  $('#buttons').on('click', '.button', placePiece)
 })
 
 // GameController base
 function gameController(view,board) {
   view = new view
   board = new board
+  var currentPlayer = ''
+  Initiate()
 }
 
 gameController.prototype = {
-  
+  Initiate: function() {
+    setListeners() 
+    setCurrentPlayer('player1')
+    Display.updateCell(row,colum,player)
+  }
+
+  setCurrentPlayer: function(player) {
+    currentPlayer = player
+    Display.updatePlayer(player)
+  }
+
+  placePiece: function(event) {
+    var columnElement = event
+    
+  }
+
+
+
 }
