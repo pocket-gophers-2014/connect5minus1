e1e99ca3a0df6cb257dae70c544c1213feedeabb
Anh Kim <akhoang88@gmail.com>
2014-05-01 17:19:29 -0700

    and selector for button and player+color of piece condition

--- source/js/views/viewBoard.js
+++ source/js/views/viewBoard.js
@@ -1,31 +1,18 @@
-function viewBoard() {
-}
+function viewBoard() {}
 
 viewBoard.prototype = {
   getCell: function(piece) {
-    //need location of the cell
-    return piece.$("div .column")
+    //find the specific cell
+    currentCell = document.querySelectorAll('.column')[piece.column].querySelectorAll('div')[piece.row];
+    return currentCell;
   },
 
   updateCell: function(piece) {
-    //needlocation of cell based on new html
-    var currentCell = $("piece.column piece.row")
     if (piece.player.id === 1) {
-      piece.css("background-color", "red")
+      currentCell.css("background-color", "red");
     }
     else {
-      piece.css("background-color", "black")
+      currentCell.css("background-color", "black");
     }
   }
 }
-
-button = $("#button-row #button-row")
-
-
-//what is shipped to me.
-//// piece obj has properties row, column, player.id
-///
-// when u get the piece obj, update the row and column id and return color
-
-//add eventlistener on the board, to know which piece is added
-// need html for this....
